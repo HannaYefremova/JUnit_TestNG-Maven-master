40496519b2387f13c4d37141270bd691f1016f3b
Hanna Yefremova <annakurdel@gmail.com>
2023-01-26 13:59:22 +0200

    Create new methods medium and hard

--- src/main/java/application/Calculator.java
+++ src/main/java/application/Calculator.java
@@ -29,8 +29,8 @@ public class Calculator {
     }
 
     //TODO add method for x^2
-    public int square(int b) {
-        return b * b;
+    public int square(int a) {
+        return a * a;
     }
 
     //***************Medium methods************************
